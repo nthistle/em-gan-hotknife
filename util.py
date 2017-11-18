ed18fba4d27cbab469071fa0de48441a1c40c102
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-11-17 21:21:06 -0500

    Add masked mse loss function

--- util.py
+++ util.py
@@ -1,6 +1,10 @@
 import numpy as np
-import keras
+from keras.losses import mean_squared_error
+from keras import backend as K
 
-
-def get_mse_masked_loss():
-	pass
+def get_mse_masked_loss(mask=None):
+	if not mask:
+		return mean_squared_error
+	def mse_masked_loss(y_true, y_pred):
+		return K.mean(K.square(mask * (y_pred - y_true)))
+	return mse_masked_loss
