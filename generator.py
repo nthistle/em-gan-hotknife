2949098bdb80db0f66df8a9b1201a0a78f708177
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-01-05 15:15:58 -0500

    Add convolutions

--- generator.py
+++ generator.py
@@ -16,15 +16,16 @@ def get_generator(shape):
 	layer = Conv3D(32, (3,3,3), activation="relu", padding="same")(layer)
 	layer = Conv3D(16, (3,3,3), activation="relu", padding="same")(layer)
 	layer = MaxPooling3D((2,2,2), padding="same")(layer)
+	layer = Conv3D(8, (3,3,3), activation="relu", padding="same")(layer)
 	encoded = layer
 	layer = Conv3D(16, (3,3,3), activation="relu", padding="same")(layer)
 	layer = UpSampling3D((2,2,2))(layer)
 	layer = Conv3D(32, (3,3,3), activation="relu", padding="same")(layer)
 	layer = Conv3D(32, (3,3,3), activation="relu", padding="same")(layer)
 	layer = UpSampling3D((2,2,2))(layer)
+	layer = Conv3D(32, (3,3,3), activation="relu", padding="same")(layer)
 	layer = Conv3D(64, (3,3,3), activation="relu", padding="same")(layer)
 	layer = Conv3D(1, (3,3,3), activation="sigmoid", padding="same")(layer)
 	decoded = layer
 	model = Model(inp, decoded)
-	print(model.summary())
 	return model
