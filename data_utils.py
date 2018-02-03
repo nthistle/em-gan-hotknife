259be8cd99811dc665ffdea74d861773453fb708
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-02-02 19:25:51 -0500

    Add pretraining stuff

---
+++ data_utils.py
@@ -0,0 +1,28 @@
+import h5py
+import numpy as np
+
+# the gap is around depth 100 (for safety, don't use from 95 to 105)
+# so, to get our 30x30x30 block, we're going to pick random depths from 0 to 130 (excl)
+# and if it's greater than or eq to 65, we add 40 to it, to safely miss the gap
+def h5_nogap_data_generator(data_filename, data_path, sample_shape, batch_size):
+
+	data = h5py.File(data_filename, "r")[data_path]
+
+	while True:
+		batch = np.empty((batch_size,) + sample_shape + (1,))
+
+		z_start = np.random.randint(0, 130, batch_size)
+		z_start = z_start + (40 * (z_start >= 65))
+
+		x_start = np.random.randint(0, data.shape[1]-sample_shape[1], batch_size)
+		y_start = np.random.randint(0, data.shape[2]-sample_shape[2], batch_size)
+
+		for k in range(batch_size):
+			data.read_direct(batch,
+				np.s_[z_start[k] : z_start[k] + sample_shape[0],
+				x_start[k] : x_start[k] + sample_shape[1],
+				y_start[k] : y_start[k] + sample_shape[2]],
+				np.s_[k, :, :, :, 0])
+
+		yield batch
+		
---
+++ pretrain.py
@@ -0,0 +1,65 @@
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D, Dense, Reshape, Flatten, Activation, Input, Lambda
+from keras.models import Sequential
+from keras.layers.advanced_activations import LeakyReLU
+from keras.losses import binary_crossentropy
+from keras.optimizers import Adam
+from keras.regularizers import L1L2
+from keras import backend as K
+from PIL import Image
+from scipy.misc import imresize
+import numpy as np
+import tensorflow as tf
+from util import *
+from discriminator import *
+from generator import *
+from data_utils import *
+
+
+def write_sampled_output(samp, outp, fname):
+	im = np.zeros((300, 300), dtype=np.uint8) # 10 cuts at even spacing, 5 samples, plus 5 outputs
+	for i in range(5):
+		for j in range(10):
+			im[60*i:60*i+30,30*j:30*(j+1)] = samp[i,j,:,:]
+			im[60*i:60*i+60,30*j:30*(j+1)] = outp[i,j,:,:]
+	Image.fromarray(imresize(im, 2.0, interp="nearest")).save(fname)
+
+
+def main(epochs=200, batch_size=1024, lr=1e-5):
+
+	print("Running pretraining with %d epochs, batch size of %d")
+	print("Learning rate is %f" % lr)
+	
+	generator_model = get_generator(shape=(30,30,30))
+
+	generator.compile(loss='binary_crossentropy', optimizer=Adam(lr))
+
+	# for sampling the data for training
+	data_gen = h5_nogap_data_generator("hotknifedata.hdf5","volumes/data", (30,30,30), batch_size)
+
+	# just for periodically sampling the generator to see what's going on
+	test_gen = h5_nogap_data_generator("hotknifedata.hdf5","volumes/data", (30,30,30), 5)
+
+	train_hist = []
+
+	for epoch in range(epochs):
+
+		samp = data_gen.__next__()
+
+		g_loss = generator.train_on_batch(samp, samp)
+		#g_loss = gen_noedge.train_on_batch(latent_samp, latent_samp)
+
+		print("Epoch #%d [G loss: %f]" % (epoch+1, g_loss))
+
+		train_hist.append(g_loss)
+
+		# now save some sample input
+		prev = test_gen.__next__()
+
+		outp = generator.predict(prev)
+
+		write_sampled_output(prev, outp, "pretrain_epoch_%03d.png"%(epoch+1))
+
+		generator.save("generator_pretrain_epoch_%03d.h5"%(epoch+1))
+
+if __name__ == "__main__":
+	main()
