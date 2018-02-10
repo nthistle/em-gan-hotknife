9e097ee5898356e44318a9de3bd8aa219031c919
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-02-09 21:07:43 -0500

    Add smaller version of discriminator

--- discriminator.py
+++ discriminator.py
@@ -8,6 +8,25 @@ from util import *
 
 ## Discriminator is similar to first half of generator, reduces to features
 ## but then it has Dense layers
+def get_discriminator_small(shape):
+	inp = Input(shape=(shape + (1,)))
+	layer = Conv3D(8, (3,3,3), activation="relu", padding="same")(inp)
+	layer = Conv3D(8, (3,3,3), activation="relu", padding="same")(layer)
+	layer = MaxPooling3D((2,2,2), padding="same")(layer)
+	layer = Conv3D(16, (3,3,3), activation="relu", padding="same")(layer)
+	layer = Conv3D(16, (3,3,3), activation="relu", padding="same")(layer)
+	layer = MaxPooling3D((2,2,2), padding="same")(layer)
+	layer = Conv3D(32, (3,3,3), activation="relu", padding="same")(layer)
+	layer = Conv3D(32, (3,3,3), activation="relu", padding="same")(layer)
+	layer = MaxPooling3D((2,2,2), padding="same")(layer)
+	layer = Conv3D(32, (3,3,3), activation="relu", padding="same")(layer)
+	# at this point it's 64 4x4x4 filters, consider doing fewer of larger
+	layer = Reshape((2048,))(layer)
+	layer = Dense(16, activation="relu")(layer)
+	layer = Dense(1, activation="sigmoid")(layer)
+	model = Model(inp, layer)
+	return model
+
 def get_discriminator(shape):
 	inp = Input(shape=(shape + (1,)))
 	layer = Conv3D(16, (3,3,3), activation="relu", padding="same")(inp)
