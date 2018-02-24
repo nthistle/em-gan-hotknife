78ef0f431a4220ae066367138d5dc00a77c6dd6a
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-02-23 20:59:30 -0500

    Add valid-padded generator

--- generator.py
+++ generator.py
@@ -6,6 +6,27 @@ import numpy as np
 from util import *
 
 
+## 64x64x64
+def get_generator_valid(shape):
+	inp = Input(shape=(shape + (1,)))  # 64^3
+	layer = Conv3D(8, (3,3,3), activation="relu")(inp) # 62^3
+	layer = Conv3D(16, (3,3,3), activation="relu")(layer) # 60^3
+	layer = MaxPooling3D((2,2,2))(layer) # 30^3
+	layer = Conv3D(32, (3,3,3), activation="relu")(layer) # 28^3
+	layer = Conv3D(48, (3,3,3), activation="relu")(layer) # 26^3
+	layer = MaxPooling3D((2,2,2))(layer) # 13^3
+	layer = Conv3D(64, (3,3,3), activation="relu")(layer) # 11^3
+	layer = UpSampling3D((2,2,2))(layer) # 22^3
+	layer = Conv3D(48, (3,3,3), activation="relu")(layer) # 20^3
+	layer = Conv3D(32, (3,3,3), activation="relu")(layer) # 18^3
+	layer = UpSampling3D((2,2,2))(layer) # 36^3
+	layer = Conv3D(16, (3,3,3), activation="relu")(layer) # 34^3
+	layer = Conv3D(8, (3,3,3), activation="relu")(layer) # 32^3
+	layer = Conv3D(1, (1,1,1), activation="sigmoid")(layer) #32^3
+	decoded = layer
+	model = Model(inp, decoded)
+	return model
+
 ## Generator has an Autoencoder architecture, maps (32,)^3 to 8092-dimensional
 ## latent space, then back to (32,)^3
 def get_generator(shape):
@@ -29,6 +50,8 @@ def get_generator(shape):
 	decoded = layer
 	model = Model(inp, decoded)
 	return model
+
+
 
 def get_generator_bad(shape):
 	inp = Input(shape=(shape + (1,)))
