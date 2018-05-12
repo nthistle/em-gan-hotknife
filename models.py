7b253cce79df21db435d19b7a0853f315180c038
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-05-11 20:45:48 -0400

    Name layers

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from keras.layers import Conv3D, Conv3DTranspose, UpSampling3D, Dense, Reshape, Flatten, Activation, Input, MaxPooling3D
+from keras.layers import Conv3D, Conv3DTranspose, UpSampling3D, Dense, Reshape, Flatten, Activation, Input, MaxPooling3D, Cropping3D, Concatenate
 from keras.models import Sequential, Model
 from keras.layers.advanced_activations import LeakyReLU
 from keras.optimizers import Adam
@@ -14,38 +14,38 @@ def get_generator(relu_leak=0.2):
 	"""
 	input_layer = Input(shape = (64,64,64,1))
 
-	conv1_1 = Conv3D(8, (3,3,3))(input_layer)
-	relu1_1 = LeakyReLU(relu_leak)(conv1_1)
-	conv1_2 = Conv3D(16, (3,3,3))(relu1_1)
-	relu1_2 = LeakyReLU(relu_leak)(conv1_2)
+	conv1_1 = Conv3D(8, (3,3,3), name="conv1_1")(input_layer)
+	relu1_1 = LeakyReLU(relu_leak, name="relu1_1")(conv1_1)
+	conv1_2 = Conv3D(16, (3,3,3), name="conv1_2")(relu1_1)
+	relu1_2 = LeakyReLU(relu_leak, name="relu1_2")(conv1_2)
 
-	pool1 = MaxPooling3D((2,2,2))(relu1_2)
+	pool1 = MaxPooling3D((2,2,2), name="pool1")(relu1_2)
 
-	conv2_1 = Conv3D(32, (3,3,3))(pool1)
-	relu2_1 = LeakyReLU(relu_leak)(conv2_1)
-	conv2_2 = Conv3D(48, (3,3,3))(relu2_1)
-	relu2_2 = LeakyReLU(relu_leak)(conv2_2)
+	conv2_1 = Conv3D(32, (3,3,3), name="conv2_1")(pool1)
+	relu2_1 = LeakyReLU(relu_leak, name="relu2_1")(conv2_1)
+	conv2_2 = Conv3D(48, (3,3,3), name="conv2_2")(relu2_1)
+	relu2_2 = LeakyReLU(relu_leak, name="relu2_2")(conv2_2)
 
-	pool2 = MaxPooling3D((2,2,2))(relu2_2)
+	pool2 = MaxPooling3D((2,2,2), name="pool2")(relu2_2)
 
-	conv3 = Conv3D(96, (3,3,3))(pool2)
-	relu3 = LeakyReLU(relu_leak)(conv3)
+	conv3 = Conv3D(96, (3,3,3), name="conv3")(pool2)
+	relu3 = LeakyReLU(relu_leak, name="relu3")(conv3)
 
-	upsamp1 = UpSampling3D((2,2,2))(relu3)
+	upsamp1 = UpSampling3D((2,2,2), name="upsamp1")(relu3)
 
-	conv4_1 = Conv3D(48, (3,3,3))(upsamp1)
-	relu4_1 = LeakyReLU(relu_leak)(conv4_1)
-	conv4_2 = Conv3D(32, (3,3,3))(relu4_1)
-	relu4_2 = LeakyReLU(relu_leak)(conv4_2)
+	conv4_1 = Conv3D(48, (3,3,3), name="conv4_1")(upsamp1)
+	relu4_1 = LeakyReLU(relu_leak, name="relu4_1")(conv4_1)
+	conv4_2 = Conv3D(32, (3,3,3), name="conv4_2")(relu4_1)
+	relu4_2 = LeakyReLU(relu_leak, name="relu4_2")(conv4_2)
 
-	upsamp2 = UpSampling3D((2,2,2))(relu4_2)
+	upsamp2 = UpSampling3D((2,2,2), name="upsamp2")(relu4_2)
 
-	conv5_1 = Conv3D(16, (3,3,3))(upsamp2)
-	relu5_1 = LeakyReLU(relu_leak)(conv5_1)
-	conv5_2 = Conv3D(8, (3,3,3))(relu5_1)
-	relu5_2 = LeakyReLU(relu_leak)(conv5_2)
+	conv5_1 = Conv3D(16, (3,3,3), name="conv5_1")(upsamp2)
+	relu5_1 = LeakyReLU(relu_leak, name="relu5_1")(conv5_1)
+	conv5_2 = Conv3D(8, (3,3,3), name="conv5_2")(relu5_1)
+	relu5_2 = LeakyReLU(relu_leak, name="relu5_2")(conv5_2)
 
-	conv5_3 = Conv3D(1, (1,1,1), activation="sigmoid")(relu5_2)
+	conv5_3 = Conv3D(1, (1,1,1), activation="sigmoid", name="conv5_3")(relu5_2)
 	output_layer = conv5_3
 
 	return Model(input_layer, output_layer)
