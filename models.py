42d020e5cecec1a699722f072570971d5c1614a1
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-05-25 19:05:11 -0400

    Add skip connections

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from keras.optimizers import Adam
 
 import numpy as np
 
-def get_generator(relu_leak=0.2):
+def get_generator(relu_leak=0.2, skip_connections=False):
 	"""Returns valid-padded small generator, mapping 64x64x64 input to 32x32x32 output.
 	Architecture is similar to an autoencoder; no skip connections.
 
@@ -14,33 +14,51 @@ def get_generator(relu_leak=0.2):
 	"""
 	input_layer = Input(shape = (64,64,64,1))
 
-	conv1_1 = Conv3D(8, (3,3,3), name="conv1_1")(input_layer)
+	stage1_in = input_layer # feeds into 'stage 1' of the network
+
+	conv1_1 = Conv3D(8, (3,3,3), name="conv1_1")(stage1_in)
 	relu1_1 = LeakyReLU(relu_leak, name="relu1_1")(conv1_1)
 	conv1_2 = Conv3D(16, (3,3,3), name="conv1_2")(relu1_1)
 	relu1_2 = LeakyReLU(relu_leak, name="relu1_2")(conv1_2)
 
 	pool1 = MaxPooling3D((2,2,2), name="pool1")(relu1_2)
 
-	conv2_1 = Conv3D(32, (3,3,3), name="conv2_1")(pool1)
+	stage2_in = pool1 # feeds into 'stage 2' of the network
+
+	conv2_1 = Conv3D(32, (3,3,3), name="conv2_1")(stage2_in)
 	relu2_1 = LeakyReLU(relu_leak, name="relu2_1")(conv2_1)
 	conv2_2 = Conv3D(48, (3,3,3), name="conv2_2")(relu2_1)
 	relu2_2 = LeakyReLU(relu_leak, name="relu2_2")(conv2_2)
 
 	pool2 = MaxPooling3D((2,2,2), name="pool2")(relu2_2)
 
-	conv3 = Conv3D(96, (3,3,3), name="conv3")(pool2)
+	stage3_in = pool2 # feeds into 'stage 3' of the network
+
+	conv3 = Conv3D(96, (3,3,3), name="conv3")(stage3_in)
 	relu3 = LeakyReLU(relu_leak, name="relu3")(conv3)
 
 	upsamp1 = UpSampling3D((2,2,2), name="upsamp1")(relu3)
 
-	conv4_1 = Conv3D(48, (3,3,3), name="conv4_1")(upsamp1)
+	stage4_in = upsamp1 # feeds into 'stage 4' of the network
+
+	if skip_connections:
+		stage2_out_cropped = Cropping3D(cropping=2)(relu2_2) # (26,)^3 -> (22,)^3
+		stage4_in = Concatenate()([stage4_in, stage2_out_cropped])
+
+	conv4_1 = Conv3D(48, (3,3,3), name="conv4_1")(stage4_in)
 	relu4_1 = LeakyReLU(relu_leak, name="relu4_1")(conv4_1)
 	conv4_2 = Conv3D(32, (3,3,3), name="conv4_2")(relu4_1)
 	relu4_2 = LeakyReLU(relu_leak, name="relu4_2")(conv4_2)
 
 	upsamp2 = UpSampling3D((2,2,2), name="upsamp2")(relu4_2)
 
-	conv5_1 = Conv3D(16, (3,3,3), name="conv5_1")(upsamp2)
+	stage5_in = upsamp2 # feeds into 'stage 5' of the network
+
+	if skip_connections:
+		stage1_out_cropped = Cropping3D(cropping=12)(relu1_2) # (60,)^3 -> (36,)^3
+		stage5_in = Concatenate()([stage5_in, stage1_out_cropped])
+
+	conv5_1 = Conv3D(16, (3,3,3), name="conv5_1")(stage5_in)
 	relu5_1 = LeakyReLU(relu_leak, name="relu5_1")(conv5_1)
 	conv5_2 = Conv3D(8, (3,3,3), name="conv5_2")(relu5_1)
 	relu5_2 = LeakyReLU(relu_leak, name="relu5_2")(conv5_2)
