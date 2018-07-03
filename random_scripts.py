aae84e298595a78e488a4c22402d6555518cefa7
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2018-07-03 17:35:53 -0400

    Add random_scripts

---
+++ random_scripts.py
@@ -0,0 +1,38 @@
+## Collection of random tools/utilities for size/shape calculation
+
+## Back calculate the input shape from provided output shape, shape decrease per
+## "stage", and up/down sampling factor for a U-Net
+def back_calc( output_shape, decrease_per=4, factor=2, stages=3 ):
+	shapes = [output_shape]
+	is_nice = True
+	for i in range(stages):
+		shapes.append(shapes[-1] + decrease_per)
+		is_nice &= (shapes[-1]%factor==0)
+		shapes.append(shapes[-1]//factor)
+	for i in range(stages):
+		shapes.append(shapes[-1] + decrease_per)
+		shapes.append(shapes[-1]*factor)
+	shapes.append(shapes[-1] + decrease_per)
+	return (shapes, is_nice)
+
+## Returns a format-friendly version of the result from back_calc
+## i.e. something that can be put into a ascii-rendering method relatively easily
+def back_calc_nice( output_shape, decrease_per=2, num_times=2, factor=2, stages=3 ):
+	last = lambda l : l[-1][-1]
+
+	cur_stage = [output_shape]
+	shapes = [cur_stage]
+
+	for _ in range(stages):
+		for _ in range(num_times):
+			cur_stage.append(last(shapes) + decrease_per)
+		cur_stage = [True, last(shapes)//factor]
+		shapes.append(cur_stage)
+	for _ in range(stages+1):
+		for _ in range(num_times):
+			cur_stage.append(last(shapes) + decrease_per)
+		cur_stage = [False, last(shapes)*factor]
+		shapes.append(cur_stage)
+	shapes.pop()
+
+	return shapes
