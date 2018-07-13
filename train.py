ba238f99a8b23e2eb026e06212d5cc21b4ca5790
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2018-07-13 14:20:49 -0400

    Rework all of training code

--- train.py
+++ train.py
@@ -1,6 +1,22 @@
+from keras.layers import Input
+from keras.models import load_model
+from keras.losses import mean_squared_error
+from keras.optimizers import Adam, SGD
+from keras.regularizers import L1L2
 
+from keras import backend as K
 
+from models import *
+from data_utils import *
 
+import numpy as np
+import tensorflow as tf
+import math
+
+import os
+import pandas
+import sys
+import argparse
 
 
 def get_masked_loss(batch_size, output_shape, mask_size, slice_index):
@@ -27,10 +43,27 @@ def get_masked_loss(batch_size, output_shape, mask_size, slice_index):
 	return masked_loss
 
 
+def apply_noise(samp, std_dev=0.03):
+	return np.clip(samp + np.random.normal(0.0, std_dev, size=samp.shape), 0.0, 1.0)
+
+
+def get_center_of_block(block, target_shape):
+	# if block is 64,64,64
+	# target is 24
+	start_pos = [(a-b)//2 for a, b in zip(block.shape[1:-1], target_shape)]
+	slices = [slice(start, start + s) for start, s in zip(start_pos, target_shape)]
+	return block[:,*slices]
+
 
 def train(generator, discriminator, generator_optimizer, discriminator_optimizer, penalty_optimizer,
-	epochs, minibatch_size, num_minibatch, instance_noise, instance_noise_std_dev, input_shape, output_shape,
-	generator_mask_size):
+	epochs, minibatch_size, num_minibatch, instance_noise, instance_noise_profile, input_shape, output_shape,
+	generator_mask_size, valid_generator, gap_generator, gap_index, base_save_dir):
+    """ Trains the given generator using all the given parameters and generators.
+
+    valid_generator -> should be a generator that returns entirely valid data of size (minibatch_size, *output_shape, 1)
+    gap_generator   -> should be a generator that returns data with a gap in the middle of size (minibatch_size, *input_shape, 1)
+
+    """
 
 	discriminator.compile(loss='binary_crossentropy', optimizer=discriminator_optimizer, metrics=['accuracy'])
 
@@ -39,198 +72,94 @@ def train(generator, discriminator, generator_optimizer, discriminator_optimizer
 
 	penalty_z = Input(shape=input_shape+(1,))
 	penalty = Model(penalty_z, generator(penalty_z))
-	penalty.compile(loss=)
+	penalty.compile(loss=get_masked_loss(minibatch_size, output_shape, generator_mask_size, gap_index), optimizer=penalty_optimizer)
 
-
-def train(generator_filename, epochs=25, batch_size=64, num_batches=32,
-	disc_lr=1e-7, gen_lr=1e-6, penalty_lr=1e-5, num_passive=2, noise=0.0,
-	disc_optim="adam", gen_optim="adam", pen_optim="adam", batch_norm=False,
-	lower_noise=False, disc_regularization=0, sc_enabled=False,
-	data_file="hotknifedata.hdf5", output_folder="run_output"):
-
-	disc_optim = disc_optim.lower()
-	gen_optim = gen_optim.lower()
-	pen_optim = pen_optim.lower()
-
-	print("Running em-hotknife GAN training for %d epochs with parameters:" % epochs)
-	print("Discriminator LR: %f" % disc_lr)
-	print("Generator LR: %f" % gen_lr)
-	print("Penalty LR: %f" % penalty_lr)
-	print("Outputting data to %s" % output_folder)
-
-	if not os.path.isdir(output_folder):
-		os.mkdir(output_folder)
-
-	discriminator_optimizer = None
-	if disc_optim == "adam":
-		discriminator_optimizer = Adam(disc_lr)
-	elif disc_optim == "sgd":
-		discriminator_optimizer = SGD(disc_lr)
-
-	generator_optimizer = None
-	if gen_optim == "adam":
-		generator_optimizer = Adam(gen_lr)
-	elif gen_optim == "sgd":
-		generator_optimizer = SGD(gen_lr)
-
-	penalty_optimizer = None
-	if pen_optim == "adam":
-		penalty_optimizer = Adam(penalty_lr)
-	elif pen_optim == "sgd":
-		penalty_optimizer = SGD(penalty_lr)
-
-	discriminator = get_discriminator(batch_norm=batch_norm, regularization=disc_regularization)
-	discriminator.compile(loss='binary_crossentropy', optimizer=discriminator_optimizer, metrics=['accuracy'])
-
-	generator = load_model(generator_filename)
-	generator.name = "pretrained_generator"
-	generator.compile(loss='binary_crossentropy', optimizer=generator_optimizer) # Fairly certain this doesn't get directly used anywhere
-
-	penalty_z = Input(shape=(64,64,64,1))
-	penalty = Model(penalty_z, generator(penalty_z))
-	penalty.compile(loss=get_masked_loss(batch_size), optimizer=penalty_optimizer)
-
-	z = Input(shape=(64,64,64,1))
-	img = generator(z)
-
+	z = Input(shape=input_shape)
+	fake_block = generator(z)
 	discriminator.trainable = False
+	disc_pred = discriminator(fake_block)
+	combined = Model(z, disc_pred)
 
-	valid = discriminator(img)
-
-	combined = Model(z, valid)
-	combined.compile(loss='binary_crossentropy', optimizer=generator_optimizer)
-
-	# for sampling the data for training
-	fake_gen = h5_gap_data_generator_valid(data_file,"volumes/data", (64,64,64), batch_size)
-
-	# for training discriminator on what is real
-	real_gen = h5_nogap_data_generator(data_file,"volumes/data", (32,32,32), batch_size)
-
-	# just for periodically sampling the generator to see what's going on
-	test_gen = h5_gap_data_generator_valid(data_file,"volumes/data", (64,64,64), 5)
-	common_test_gen = h5_gap_data_generator_valid(data_file, "volumes/data", (64,64,64), 15)
-
-	common_test = common_test_gen.__next__()  ## Will sample this same one every epoch to better see what it's learning
-
-
-	## Once we've had 3+ "critical" epochs (where discriminator loss is much lower than generator loss), terminate training early, if short circuiting enabled
-	CRITICAL_EPOCH_THRESH = 3
-	critical_epochs = 0
+	test_sample = gap_generator.__next__()
 
 	history = {"epoch":[], "d_loss":[], "d_acc":[], "g_loss":[], "g_penalty":[]}
 
-	## Just do an "Epoch 0" test
-	latent_samp = fake_gen.__next__()
-	gen_output = generator.predict(latent_samp)
-	real_data = real_gen.__next__()
-	d_loss = 0.5 * np.add(discriminator.test_on_batch(real_data, np.ones((batch_size, 1))), discriminator.test_on_batch(gen_output, np.zeros((batch_size, 1))))
-	g_loss = combined.test_on_batch(latent_samp, np.ones((batch_size, 1)))
-	g_loss_penalty = penalty.test_on_batch(latent_samp, get_center_of_valid_block(latent_samp))
-	print("Epoch #0 [D loss: %f acc: %f] [G loss: %f penalty: %f]" % (d_loss[0], d_loss[1], g_loss, g_loss_penalty))
-	print("="*50)
+	if not os.path.exists(os.path.join(base_save_dir, "model-saves")):
+		os.makedirs(os.path.join(base_save_dir, "model-saves"))
 
-	history["epoch"].append(0)
-	history["d_loss"].append(d_loss[0])
-	history["d_acc"].append(d_loss[1])
-	history["g_loss"].append(g_loss)
-	history["g_penalty"].append(g_loss_penalty)
-	## End our Epoch 0 stuff
+	if not os.path.exists(os.path.join(base_save_dir, "samples")):
+		os.makedirs(os.path.join(base_save_dir, "samples"))
 
-	current_noise = noise
 
-	for epoch in range(epochs):
-
-		if lower_noise:
-			current_noise = max(min(noise * (1 - 2*(epoch-(epochs/4))/epochs),1),0)
-		
-		g_loss = None
-		g_loss_penalty = None
-		d_loss = None
-
-		for n in range(num_batches): # do n minibatches
-
-			# train discriminator
-			latent_samp = fake_gen.__next__() # input to generator
-			gen_output = generator.predict(latent_samp)
-
-			if current_noise > 0.0:
-				gen_output = apply_noise(gen_output, current_noise) # instance noise, sorta
-			# hopefully helps a little bit?
-
-			real_data = real_gen.__next__()
-
-			d_loss_real = discriminator.train_on_batch(real_data, np.ones((batch_size, 1)))
-			d_loss_fake = discriminator.train_on_batch(gen_output, np.zeros((batch_size, 1)))
-			d_loss_new = (1./num_batches) * 0.5 * np.add(d_loss_real, d_loss_fake)
-
-			if d_loss is None:
-				d_loss = d_loss_new
-			else:
-				d_loss = np.add(d_loss, d_loss_new)
-
-			# train generator
-			latent_samp = fake_gen.__next__()
-
-			if epoch < num_passive:
-				g_loss_new = (1./num_batches) * combined.test_on_batch(latent_samp, np.ones((batch_size, 1)))
-			else:
-				g_loss_new = (1./num_batches) * combined.train_on_batch(latent_samp, np.ones((batch_size, 1)))
-
-			## Now penalty instead of generator
-			if epoch < num_passive:
-				g_loss_penalty_new = (1./num_batches) * penalty.test_on_batch(latent_samp, get_center_of_valid_block(latent_samp))
-			else:
-				g_loss_penalty_new = (1./num_batches) * penalty.train_on_batch(latent_samp, get_center_of_valid_block(latent_samp))
-
-			if g_loss is None:
-				g_loss = g_loss_new
-				g_loss_penalty = g_loss_penalty_new
-			else:
-				g_loss = np.add(g_loss, g_loss_new)
-				g_loss_penalty = np.add(g_loss_penalty, g_loss_penalty_new)
-
-		print("Epoch #%d [D loss: %f acc: %f] [G loss: %f penalty: %f]" % (epoch+1, d_loss[0], d_loss[1], g_loss, g_loss_penalty))
-
-		if epoch > num_passive + CRITICAL_EPOCH_THRESH and (g_loss/d_loss[0]) > 2.0:
-			critical_epochs += 1
-		else:
-			critical_epochs = 0
-
-		# now save some sample input
-		#prev = test_gen.__next__()
-
-		#outp = generator.predict(prev)
-		common_outp = generator.predict(common_test)
-
-		#if noise > 0.0:
-		#	outp = apply_noise(outp, noise)
-		#	common_outp = apply_noise(common_outp, noise)
-
-		#write_sampled_output_even(prev, outp, os.path.join(output_folder,"train_epoch_%03d.png"%(epoch+1)))
-		write_sampled_output_even_large(common_test, common_outp, os.path.join(output_folder,"test_epoch_%03d.png"%(epoch+1)), 32)
-
-		if (epoch+1)%5 == 0:
-			generator.save(os.path.join(output_folder,"generator_train_epoch_%03d.h5"%(epoch+1)))
-			discriminator.save(os.path.join(output_folder,"discriminator_train_epoch_%03d.h5"%(epoch+1)))
-
-		# Update History
-		history["epoch"].append(epoch+1)
-		history["d_loss"].append(d_loss[0])
-		history["d_acc"].append(d_loss[1])
+	def update_and_print_history(epoch, d_loss, d_acc, g_loss, g_penalty):
+		history["epoch"].append(epoch)
+		history["d_loss"].append(d_loss)
+		history["d_acc"].append(d_acc)
 		history["g_loss"].append(g_loss)
-		history["g_penalty"].append(g_loss_penalty)
-
-		if critical_epochs > CRITICAL_EPOCH_THRESH and sc_enabled:
-			print("[INFO] SHORT CIRCUITING TRAINING! g_loss > 2*d_loss for 3+ epochs!")
-			break
-
-	generator.save(os.path.join(output_folder,"generator_train_final.h5"))
-	discriminator.save(os.path.join(output_folder,"discriminator_train_final.h5"))
-
-	with open(os.path.join(output_folder,"history.csv"),"w") as f:
-		pandas.DataFrame(history).reindex(columns=["epoch","d_loss","d_acc","g_loss","g_penalty"]).to_csv(f, index=False)
+		history["g_penalty"].append(g_penalty)
+		print(f"Epoch #{epoch+1} [D]: loss: {d_loss[0]} acc: {d_loss[1]}, [G]: loss: {g_loss} penalty: {g_loss_penalty}")
 
 
+	persistent_sample = np.zeros((18, *input_shape, 1))
+	for i in range(math.ceil(18/minibatch_size)):
+		samp = gap_generator.__next__()
+		persistent_sample[i:min(i+minibatch_size,18)] = samp[:min(minibatch_size,18-i*minibatch_size)]
 
-if __name__=="__main__":
-	main()
+	def sample_and_write_output(output_directory, epoch, width=32):
+		block_height = output_shape[(gap_index+1)%3]
+		block_length = output_shape[(gap_index+2)%3]
+		slices = [slice()]*3
+		im = np.zeros((18*2*block_height, width*block_length))
+		#sample_prediction = np.zeros((18, *output_shape, 1))
+		for i in range(18): ## TODO: do this in minibatches
+			sample_prediction = generator.predict(persistent_sample[i:i+1])[0]
+			for j in range(width):
+				slices[gap_index] = slice(round(j*output_shape[gap_index]/width),round(j*output_shape[gap_index]/width)+1)
+				im[i*2*block_height:(i*2+1)*block_height,block_length*j:block_length*(j+1)] = sample_prediction[slices[0], slices[1], slices[2], 0]
+		Image.fromarray(np.clip((255*im).round(),0,255).astype(np.uint8)).save(os.path.join(output_directory, "sample_epoch_%03d.png" % epoch))
+
+
+	for epoch in range(1,epochs+1):
+
+		d_loss, g_loss, g_loss_penalty = None, None, None
+
+		for n in range(num_minibatch):
+			
+			## Train the Discriminator
+			gap_data = gap_generator.__next__()
+			gen_output = generator.predict(gap_data)
+
+			if instance_noise:
+				gen_output = apply_noise(gen_output, instance_noise_profile[epoch])
+
+			valid_data = valid_generator.__next__()
+
+			d_loss_real = discriminator.train_on_batch(valid_data, np.ones((minibatch_size, 1)))
+			d_loss_fake = discriminator.train_on_batch(gen_output, np.zeros((minibatch_size, 1)))
+			d_loss_new = (1./num_minibatch) * 0.5 * np.add(d_loss_real, d_loss_fake)
+
+
+			## Train the Generator
+			gap_data = gap_generator.__next__()
+
+			## Through the Discriminator
+			g_loss_new = (1./num_minibatch) * combined.train_on_batch(gap_data, np.ones((minibatch_size)))
+
+			## Penalty Training
+			g_loss_penalty_new = (1./num_minibatch) * penalty.train_on_batch(gap_data, get_center_of_block(gap_data, output_shape))
+
+
+			## Record Losses
+			d_loss = d_loss_new if d_loss is None else np.add(d_loss, d_loss_new)
+			g_loss, g_loss_penalty = g_loss_new, g_loss_penalty_new if g_loss is None else np.add(g_loss, g_loss_new), np.add(g_loss_penalty, g_loss_penalty_new)
+
+
+		update_and_print_history(epoch=epoch, d_loss=d_loss[0], d_acc=d_loss[1], g_loss=g_loss, g_penalty=g_loss_penalty)
+
+		sample_and_write_output(output_directory=os.path.join(base_save_dir, "samples"))
+
+		if (epoch)%15 == 0:
+			generator.save(os.path.join(base_save_dir,"model-saves","generator_train_epoch_%03d.h5"%(epoch+1)))
+			discriminator.save(os.path.join(base_save_dir,"model-saves","discriminator_train_epoch_%03d.h5"%(epoch+1)))
+
+	return generator
