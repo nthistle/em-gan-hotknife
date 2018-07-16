18c5e981f61e918c5cf71575e1e94657b6734bb0
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2018-07-16 16:02:28 -0400

    Implement handling of pretrain

--- run_model.py
+++ run_model.py
@@ -7,10 +7,10 @@ from keras.regularizers import L1L2
 
 from keras import backend as K
 
-from models import *
-
-from train import *
-#from pretrain import *
+import models
+import train
+import pretrain
+import data_utils
 
 import numpy as np
 import tensorflow as tf
@@ -21,8 +21,6 @@ import sys
 
 import argparse
 import configparser
-
-
 
 def get_argparser():
 	parser = argparse.ArgumentParser(description="em-gan-hotknife")
@@ -38,7 +36,28 @@ def str2bool(v, var=None):
 		raise argparse.ArgumentTypeError('Boolean value expected%s!' % ('for variable %s' % var if var else ''))
 
 def handle_pretrain(global_args, pretrain_args):
-	pass
+	architecture_specs = models.ARCHITECTURES["generator"][pretrain_args["generator_architecture"]]
+
+	generator_constructor_args = {arg[14:]:pretrain_args[arg] for arg in pretrain_args if arg[:14]=="generator_arg_"}
+	generator = architecture_specs[0](**generator_constructor_args) #invoke constructor with provided args
+
+	generator_optimizer = Adam(lr=float(pretrain_args["generator_learning_rate"]))
+	num_epochs = int(pretrain_args["num_epochs"])
+	num_minibatch = int(pretrain_args["num_minibatch"])
+	minibatch_size = int(pretrain_args["minibatch_size"])
+	input_shape = architecture_specs[1]
+	output_shape = architecture_specs[2]
+
+	base_save_dir = os.path.join(global_args["run_output"],"pretrain")
+	os.makedirs(base_save_dir)
+
+	valid_data_generator = data_utils.valid_data_generator_n5(global_args["valid_container"], global_args["valid_dataset"], input_shape, minibatch_size)
+
+	pretrain.pretrain(generator=generator, generator_optimizer=generator_optimizer, epochs=num_epochs,
+		minibatch_size=minibatch_size, num_minibatch=num_minibatch, input_shape=input_shape, output_shape=output_shape,
+		valid_generator=valid_data_generator, base_save_dir=base_save_dir)
+
+
 
 def handle_train(generator, global_args, train_args):
 	""" Handles the main training of the model. Does the busywork/cleaning up needed to call train.train().
